f2a08851909f8d40ba22c3be71ec118197bd172d
arzaw <arzaw_11815115@nitkkr.ac.in>
2021-12-17 02:00:15 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
   { path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'users', component: UsersComponent},
+  { path: 'users/:id/:name', component: UserComponent},
   { path: 'servers', component: ServersComponent}
 ]
 
