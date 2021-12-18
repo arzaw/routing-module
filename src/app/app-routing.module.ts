74247adb8a941f94ce96d2f333f58a59feaf312a
arzaw <arzaw_11815115@nitkkr.ac.in>
2021-12-18 14:58:07 +0530

    router ending

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Router, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from "./auth-guard.service";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { canDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -14,9 +16,14 @@ const appRoutes: Routes = [
     { path: 'users', component: UsersComponent, children: [
       { path: ':id/:name', component: UserComponent}
     ]},
-    { path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent},
-      { path: ':id/edit', component: EditServerComponent}
+    { 
+        path: 'servers', 
+        //canActivate: [AuthGaurd],
+        canActivateChild: [AuthGuard], 
+        component: ServersComponent, 
+        children: [
+        { path: ':id', component: ServerComponent},
+        { path: ':id/edit', component: EditServerComponent, canDeactivate: [canDeactivateGuard]}
     ]},
     { path: 'not-found', component: PageNotFoundComponent},
     { path: '**', redirectTo: '/not-found'} //**  should be kept in the last warna kaam nhi krgea
